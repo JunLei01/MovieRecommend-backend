cf4640a70b9a70ae5aa7a14153844e07bb821139
JunLei01 <2311146769@qq.com>
2022-07-03 09:15:23 +0800

    README file

---
+++ MRApp/collaborative_filtering.py
@@ -0,0 +1,229 @@
+import numpy as np
+import pandas as pd
+
+from gensim.corpora import Dictionary
+from gensim.models import TfidfModel
+
+
+def get_movie_dataset(movie_path):
+    # 读取电影数据
+    # number, name, year, director, screenwriter, roles, style, countries, language, date, long, imdb, evaluation
+    print(5)
+    movie_tags = pd.read_csv(movie_path, usecols=[1, 3, 4, 5, 6, 7, 8, 12]).dropna()
+    print(6)
+    movies = movie_tags.values.tolist()
+    print(3)
+    movie_dataset = []
+    print(0000000000)
+    # for i in range(1200,1300):
+    #     print(i+1,movies[i][0])
+    print(1)
+    for movie in movies:
+        temp = [movie[0]]
+        for _tags in movie[1:5]:
+            _tags = _tags[1:-1].replace('\'', '').replace(' ', '').split(',')
+            for _tag in _tags:
+                temp.append(_tag)
+        for _tags in movie[5:7]:
+            _tags = _tags.replace(' ', '').split('/')
+            for _tag in _tags:
+                temp.append(_tag)
+        temp.append(movie[7])
+        movie_dataset.append(temp)
+    # print(movie_dataset)
+    print(2)
+    return movie_dataset
+
+
+def create_movie_profile(movie_dataset):
+    dataset = []
+    for data in movie_dataset:
+        dataset.append(data[1:-1])
+    dct = Dictionary(dataset)
+    # print(dct)
+    corpus = [dct.doc2bow(line) for line in dataset]
+    # print(corpus)
+    model = TfidfModel(corpus)
+    # print(model)
+    # print(model[corpus[0]])
+    _movie_profile = []
+    for i in range(len(movie_dataset)):
+        data = movie_dataset[i]
+        name = data[0]
+        # print(data[-1])
+        try:
+            score = float(data[-1])
+        except:
+            score = float(5)
+        # print(score)
+        vector = model[corpus[i]]
+        # print(vector)
+        movie_tags_weights = dict(map(lambda x: (dct[x[0]], x[1] * score), vector))
+        # print(movie_tags_weights)
+        movie_tags = [i[0] for i in movie_tags_weights.items()]
+        # print(movie_tags)
+        _movie_profile.append((i + 1, name, movie_tags, movie_tags_weights))
+        # print(i+1,name)
+        # print(_movie_profile)
+    movie_profile = pd.DataFrame(_movie_profile, columns=["number", "name", "profile", "weights"])
+    movie_profile.set_index("number", inplace=True)  # 修改此处可实现id与名字的转换
+    # print(movie_profile)
+    # print(movie_profile["weights"])
+    return movie_profile
+
+
+def create_inverted_table(movie_profile):
+    inverted_table = {}
+    for mid, weights in movie_profile['weights'].iteritems():
+        # print(mid, weights)
+        for tag, weight in weights.items():
+            _ = inverted_table.get(tag, [])
+            _.append((mid, weight))
+            inverted_table.setdefault(tag, _)
+    # print(inverted_table)
+    return inverted_table
+
+
+def create_user_profile(movies, movie_profile):
+    # movies = [[1, 2], [1, 2], [(OB324390,1, 6.0), (OB324390,2, 8.0)]]
+    # watch_record = pd.read_csv(watch_path, usecols=range(3),
+    #                            dtype={"userId": np.int32, "movieId": np.int32, "rating": np.float32})
+    # watch_record = watch_record.groupby('userId').agg(list)
+    user_profile = {}
+    # print(watch_record)
+    surf_weights,fav_weights,score_weights = 0.3, 1, 0.8
+    surf_movie = movies[0]
+    fav_movie = movies[1]
+    score_movie = movies[2]
+    score_movie = pd.DataFrame(score_movie)
+    score_movie = score_movie.groupby(0).agg(list)
+    # print(score_movie)
+    for userId, movieIds, rating in score_movie.itertuples():
+        # print(movieIds)
+        movieIds = np.array(movieIds)
+        rating = np.array(rating)
+        user_mean = rating.mean()
+        # print(user_mean)
+        data_set = np.where(rating >= user_mean)
+        # print(data_set)
+        final_movieIds = movieIds[data_set]
+        # print(final_movieIds)
+        user_tag_weight = {}
+        for movieId in final_movieIds:
+            try:
+                movie_data_dict = movie_profile.loc[movieId]['weights']
+                # print(movie_data_dict)
+                for tag, weight in movie_data_dict.items():
+                    # print(tag, weight)
+                    if tag in user_tag_weight.keys():
+                        user_tag_weight[tag] = (user_tag_weight[tag] + weight)*score_weights
+                    else:
+                        user_tag_weight[tag] = weight*score_weights
+            except:
+                print("查无此电影！")  # 此行不会运行，运行到就是数据有问题
+        for movieId in surf_movie:
+            try:
+                movie_data_dict = movie_profile.loc[movieId]['weights']
+                # print(movie_data_dict)
+                for tag, weight in movie_data_dict.items():
+                    # print(tag, weight)
+                    if tag in user_tag_weight.keys():
+                        user_tag_weight[tag] = (user_tag_weight[tag] + weight)*surf_weights
+                    else:
+                        user_tag_weight[tag] = weight*surf_weights
+            except:
+                print("查无此电影！")  # 此行不会运行，运行到就是数据有问题
+        for movieId in fav_movie:
+            try:
+                movie_data_dict = movie_profile.loc[movieId]['weights']
+                # print(movie_data_dict)
+                for tag, weight in movie_data_dict.items():
+                    # print(tag, weight)
+                    if tag in user_tag_weight.keys():
+                        user_tag_weight[tag] = (user_tag_weight[tag] + weight)*fav_weights
+                    else:
+                        user_tag_weight[tag] = weight*fav_weights
+            except:
+                print("查无此电影！")  # 此行不会运行，运行到就是数据有问题
+        # print(user_tag_weight)
+        # user_tags = sorted(user_tag_weight.items(), key=lambda x: x[1], reverse=True)[:50]
+        user_tags = sorted(user_tag_weight.items(), key=lambda x: x[1], reverse=True)
+        # print(user_tags)
+        user_profile[userId] = user_tags
+        # print(movieIds)
+        return user_profile, movieIds
+
+
+def user_recommend_top_N(user_profile, movieIds, inverted_table):
+    user_movie_profile = {}
+    # print(user_profile)
+    for userId, tags in user_profile.items():
+        # print(userId)
+        movie_weight_dict = {}
+        for tags_weight in tags:
+            tag = tags_weight[0]
+            t_weight = tags_weight[1]
+            movie_weight_list = inverted_table[tag]
+            for movie_weight in movie_weight_list:
+                mid = movie_weight[0]
+                m_weight = movie_weight[1]
+                # print(movie_weight_dict)
+                if mid in movie_weight_dict.keys():
+                    movie_weight_dict[mid] += (t_weight * m_weight)
+                else:
+                    movie_weight_dict[mid] = (t_weight * m_weight)
+        # print(movie_weight_dict)
+        for movie in movieIds:
+            movie_weight_dict.pop(movie, None)
+        movie_weight_dict = sorted(movie_weight_dict.items(), key=lambda x: x[1], reverse=True)[:10]
+        # print(movie_weight_dict)
+        final_movieId = []
+        for movie in movie_weight_dict:
+            final_movieId.append(movie[0])
+        user_movie_profile[userId] = final_movieId
+    return user_movie_profile
+
+
+def filmEvaluation(movie_profile, inverted_table):
+    training_path = './data/train_data.csv'
+    evaluation_path = './data/rating.csv'
+    inverted_table = create_inverted_table(movie_profile)
+    user_profile, movieIds = create_user_profile(training_path, movie_profile)
+    user_recommend = user_recommend_top_N(user_profile, movieIds, inverted_table)
+    print(user_recommend[1])
+    name = []
+    for item in user_recommend[1]:
+        name.append(item[0])
+    # print(name)
+    ceshi = ['222']
+    result = set(name) & set(ceshi)
+    print(result)
+
+
+def recommend(movies):
+    movie_path = '../data/movie_info.csv'
+
+    # 获取数据，处理数据集
+
+    movie_dataset = get_movie_dataset(movie_path)
+    # 对电影构建画像
+
+    movie_profile = create_movie_profile(movie_dataset)
+    # 创建倒排表
+    print(33333333)
+    inverted_table = create_inverted_table(movie_profile)
+    # 构建用户画像
+    print(4444444444)
+    user_profile, movieIds = create_user_profile(movies, movie_profile)
+    # 对用户推荐电影
+    print(555555)
+    user_recommend = user_recommend_top_N(user_profile, movieIds, inverted_table)
+    print(6666666666)
+    return user_recommend
+    # movie = user_recommend[1]
+    # print(movie)
+    # filmEvaluation(movie_profile, inverted_table)
+
+
+# if __name__ == '__main__':
+#     recommend(movies=[[1, 2], [1, 2], [(1, 6.0), (2, 8.0)]])
--- MRApp/views.py
+++ MRApp/views.py
@@ -1,10 +1,12 @@
+import numpy as np
 from django.http import JsonResponse, HttpResponse
 from django.shortcuts import render
 from django.views import View
 import string
 import random
-from MRApp.models import Userinformation, Registerinfo, Movieinformation
+from MRApp.models import *
 from django.contrib.auth import login
+from MRApp.collaborative_filtering import recommend
 # Create your views here.
 
 
@@ -27,6 +29,8 @@ class LoginView(View):
                         type_recommend = self.type_movie_info(information['user_hobby'])
                         first = 1
                     print(type_recommend, first)
+                    # recommend = self.movie_recommend(information.get('user_account_id'))
+                    print(recommend)
                     response = {
                         'code': 200,
                         'message': "登录成功!",
@@ -57,14 +61,40 @@ class LoginView(View):
         for type in types:
             for movie in minfo:
                 styles = movie["movie_style"][1:-1].replace("'", "").replace(" ", "").split(',')
-                print(type, styles)
                 for style in styles:
                     if style == type:
                         movie_info.append(movie)
-                        if len(movie_info) == 9:
+                        if len(movie_info) == 40:
                             return movie_info
         if not movie_info:
             return "NULL"
+
+
+    def movie_recommend(self, userID):
+        movies = []
+        surf_all = Surfinformation.objects.filter(user=userID).values()
+        surf_movie = []
+        for surf in surf_all:
+            surf_movie.append(np.int32(surf['movie_id']))
+        movies.append(surf_movie)
+        fav_all = Favoriteinformation.objects.filter(user=userID).values()
+        fav_movie = []
+        for fav in fav_all:
+            fav_movie.append(np.int32(fav['movie_id']))
+        movies.append(fav_movie)
+        sco_all = Scoreinformation.objects.filter(user=userID).values()
+        sco_movie = []
+        for sco in sco_all:
+            sco_movie.append((userID, np.int32(sco["movie_id"]), np.float32(sco["movie_score"])))
+        movies.append(sco_movie)
+        print(movies)
+        movie_recommend = recommend(movies)[userID]
+        final_movie = []
+        print(1111111111)
+        for movie_id in movie_recommend:
+            final_movie.append(Movieinformation.objects.filter(movie_id=movie_id).values()[0])
+        return final_movie
+
 
 class RegisterView(View):
     def post(self, request):
@@ -107,3 +137,171 @@ class Complete(View):
             print(image)
         except:
             pass
+
+
+
+class get_movie_info(View):
+    def post(self, request):
+        try:
+            movieID = request.POST.get('id')
+            print(movieID, type(movieID))
+            movie = Movieinformation.objects.filter(movie_id=movieID).values()
+            print(movie)
+            if movie:
+                movie_info = list(movie)
+                name = movie_info[0].get('movie_name') + " "
+                print(name, 111111)
+                comments = Moviecomments.objects.filter(movie_name=name).values()
+
+                print(comments)
+
+                return JsonResponse({'code': 200, 'movie': movie_info[0]})
+        except:
+            print(2222222)
+            pass
+
+
+def get_all_movie(request):
+    try:
+        type = request.POST.get('type')
+        year = request.POST.get('year')
+        region = request.POST.get('region')
+        movies = Movieinformation.objects.all().values()
+        item = []
+        for movie in movies:
+            styles = movie["movie_style"][1:-1].replace("'", "").replace(" ", "").split(',')
+            if type in styles or type == "全部类型":
+                if year == "全部年份" or year == movie['movie_year']:
+                    if region == "全部国家" or region == movie["movie_country"]:
+                        item.append(movie)
+                    else:
+                        continue
+                else:
+                    continue
+            else:
+                continue
+        print(item)
+    except:
+        pass
+
+#
+#
+#
+#
+#
+# def movie_info(request):#电影信息
+#     surf_all = Movieinformation.objects.all().values()
+#     surf_movie = []
+#     for surf in surf_all:
+#         surf_movie.append(np.int32(surf['movie_id']))
+#     # print(surf_movie)
+#     surf_movies = []
+#     for mid in surf_movie[:100]:
+#         tmp = Movieinformation.objects.filter(movie_id=mid).values()[0]
+#         if len(tmp["movie_name"]) > 10:
+#             tmp["movie_name"] = tmp["movie_name"][:9] + '...'
+#         surf_movies.append(tmp)
+#
+#     return render(request, 'Surf_movie.html', {'surfmovie': surf_movies})
+#
+# def surf_movie(request):#用户浏览信息
+#     uid = 'OB324390'
+#     surf_all = Surfinformation.objects.filter(user_id=uid).values()
+#     surf_movie = []
+#     for surf in surf_all:
+#         surf_movie.append(np.int32(surf['movie_id']))
+#     # print(surf_movie)
+#     surf_movies = []
+#     for mid in surf_movie:
+#         # movie_info=Movieinformation.objects.filter(movie_id=mid).values()[0]
+#         # name=movie_info["user_name"]
+#         tmp = Movieinformation.objects.filter(movie_id=mid).values()[0]
+#         if len(tmp["movie_name"]) > 10:
+#             tmp["movie_name"] = tmp["movie_name"][:9] + '...'
+#         surf_movies.append(tmp)
+#
+#     return render(request, 'Surf_movie.html', {'surfmovie':surf_movies})
+#
+# def favo_movie(request):  # 用户收藏信息
+#     uid = 'OB324390'
+#     fav_all = Favoriteinformation.objects.filter(user_id=uid).values()
+#     fav_movie = []
+#     for surf in fav_all:
+#         fav_movie.append(np.int32(surf['movie_id']))
+#     # print(surf_movie)
+#     favo_movies = []
+#     for mid in fav_movie:
+#         tmp = Movieinformation.objects.filter(movie_id=mid).values()[0]
+#         if len(tmp["movie_name"]) > 10:
+#             tmp["movie_name"] = tmp["movie_name"][:9] + '...'
+#         favo_movies.append(tmp)
+#
+#     return render(request, 'Favo_movie.html', {'favomovie': favo_movies})
+#
+#
+# def eva_movie(request):  # 用户评分信息
+#     uid = 'OB324390'
+#     eva_all = Scoreinformation.objects.filter(user_id=uid).values()
+#     eva_movie = []
+#     for surf in eva_all:
+#         eva_movie.append(np.int32(surf['movie_id']))
+#     # print(surf_movie)
+#     eva_movies = []
+#     for mid in eva_movie:
+#         tmp = Movieinformation.objects.filter(movie_id=mid).values()[0]
+#         if len(tmp["movie_name"]) > 10:
+#             tmp["movie_name"] = tmp["movie_name"][:9] + '...'
+#         eva_movies.append(tmp)
+#
+#     return render(request, 'Eva_movie.html', {'evamovie': eva_movies})
+#
+#
+# def user_info(request):
+#     # 用户个人信息
+#     uid = 'OB324390'
+#
+#     u_info=Userinformation.objects.filter(user_account=uid).values()
+#     user_infos = []
+#     user_infos.append(u_info[0])
+#
+#     # 用户浏览信息
+#     surf_all = Surfinformation.objects.filter(user_id=uid).values()
+#     surf_movie = []
+#     for surf in surf_all:
+#         surf_movie.append(np.int32(surf['movie_id']))
+#     # print(surf_movie)
+#     surf_movies = []
+#     for mid in surf_movie[:6]:
+#         tmp = Movieinformation.objects.filter(movie_id=mid).values()[0]
+#         if len(tmp["movie_name"]) > 10:
+#             tmp["movie_name"] = tmp["movie_name"][:9] + '...'
+#         surf_movies.append(tmp)
+#
+#     # 用户收藏信息
+#     favo_all = Favoriteinformation.objects.filter(user_id=uid).values()
+#     favo_movie = []
+#     for favo in favo_all:
+#         favo_movie.append(np.int32(favo['movie_id']))
+#     # print(surf_movie)
+#     favo_movies = []
+#     for mid in favo_movie[:6]:
+#         tmp = Movieinformation.objects.filter(movie_id=mid).values()[0]
+#         if len(tmp["movie_name"]) > 10:
+#             tmp["movie_name"] = tmp["movie_name"][:9] + '...'
+#         favo_movies.append(tmp)
+#
+#     # 用户评分信息
+#     # eva_all = Scoreinformation.objects.filter(user_id=uid).values()
+#     # eva_movie = []
+#     # for eva in eva_all:
+#     #     eva_movie.append(np.int32(eva['movie_id']))
+#     # # print(surf_movie)
+#     eva_movies = []
+#     for mid in eva_movie[:3]:
+#         tmp = Movieinformation.objects.filter(movie_id=mid).values()[0]
+#         if len(tmp["movie_name"]) > 10:
+#             tmp["movie_name"] = tmp["movie_name"][:9] + '...'
+#         eva_movies.append(tmp)
+#
+#     return render(request, 'Personal_center.html', {'userinfo':user_infos,'surfmovie':surf_movies,'favomovie':favo_movies,'evamovie':eva_movies})
+#
